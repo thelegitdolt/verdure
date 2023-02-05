1e5e2dc4b6f1a3243af90775d95ff061ddad0f46
TheLegitDolt <asphaltpotatofy@gmail.com>
2023-02-04 21:36:18 -0800

    stuff

--- src/main/java/com/teamtaiga/verdure/Stuff/World/feature/DaisyFeature.java
+++ src/main/java/com/teamtaiga/verdure/Stuff/World/feature/DaisyFeature.java
@@ -3,19 +3,10 @@ package com.teamtaiga.verdure.Stuff.World.feature;
 import com.mojang.serialization.Codec;
 import com.teamtaiga.verdure.Stuff.World.DaisyPatchConfig;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.core.Holder;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.WorldGenLevel;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
-import net.minecraft.world.level.levelgen.feature.SimpleBlockFeature;
-import net.minecraft.world.level.levelgen.feature.configurations.SimpleBlockConfiguration;
-import net.minecraft.world.level.levelgen.placement.PlacedFeature;
-
-import java.util.Random;
 
 public class DaisyFeature extends Feature<DaisyPatchConfig> {
     public DaisyFeature(Codec<DaisyPatchConfig> codec) {
@@ -35,22 +26,25 @@ public class DaisyFeature extends Feature<DaisyPatchConfig> {
         int ySpread = config.ySpread() + 1;
         BlockPos.MutableBlockPos poss = new BlockPos.MutableBlockPos();
 
-        for (int j = 0; j < tries * 4; j ++) {
+        for (int j = 0; j < tries; j ++) {
             poss.setWithOffset(origin, random.nextInt(xzSpread) - random.nextInt(xzSpread), random.nextInt(ySpread) - random.nextInt(ySpread), random.nextInt(xzSpread) - random.nextInt(xzSpread));
             int hello = random.nextInt(64);
             if (hello < 24) {
                 if (config.daisyType().value().place(level, context.chunkGenerator(), random, poss)) {
                     ++i;
                 }
-            } else if (hello < 36) {
+            }
+            else if (hello < 36) {
                 if (config.smallFlowerType().value().place(level, context.chunkGenerator(), random, poss)) {
                     ++i;
                 }
-            } else if (hello < 52) {
+            }
+            else if (hello < 52) {
                 if (config.shortFeature().value().place(level, context.chunkGenerator(), random, poss)) {
                     ++i;
                 }
-            } else if (hello <= 64) {
+            }
+            else {
                 if (config.tallFeature().value().place(level, context.chunkGenerator(), random, poss)) {
                     ++i;
                 }
